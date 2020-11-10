e3bee772536f9a6213adb5d8263e29bca191021e
dan-adekoya <60848550+dan-adekoya@users.noreply.github.com>
2020-11-10 05:15:29 +0100

    nav ui

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,17 @@
 <template>
-    <div>
-        nav
+    <div class="nav">
+        <div class="nav_link active">
+            <p>Top</p>
+            <div class="nav_link_line"></div>
+        </div>
+        <div class="nav_link">
+            <p>About</p>
+            <div class="nav_link_line"></div>
+        </div>
+        <div class="nav_link">
+            <p>Contact</p>
+            <div class="nav_link_line"></div>
+        </div>
     </div>
 </template>
 
@@ -9,5 +20,32 @@
 </script>
 
 <style scoped>
-
+.nav{
+    position: fixed;
+    top: 50%;
+    right: -1%;
+    transform: translateY(-50%);
+}
+.nav_link{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 10px;
+    cursor: pointer;
+}
+.nav_link_line{
+    width: 50px;
+    height: 3px;
+    margin-right: 10px;
+    background: white;
+}
+.nav_link p{
+    opacity: 0;
+}
+.nav_link.active p{
+    opacity: 1;
+}
+.nav_link.active .nav_link_line{
+    width: 25px;
+}
 </style>
