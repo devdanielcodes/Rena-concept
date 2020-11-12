ec0d577f53ca65b5cfebfc2a06114af9e32a56d6
dan-adekoya <60848550+dan-adekoya@users.noreply.github.com>
2020-11-12 12:30:12 +0100

    restart

--- src/components/About.vue
+++ src/components/About.vue
@@ -30,6 +30,7 @@ export default {
                 ease: 'expo',
                 scrollTrigger:{
                     trigger: '.about',
+                    toggleActions: 'restart',   
                     start: 'top center',
                 } 
             })
